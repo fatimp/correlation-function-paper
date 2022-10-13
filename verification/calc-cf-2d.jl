d610bf1b1c9446ebf6f61c717696adebc5e98a0c
Vasily Postnicov <shamaz.mazum@gmail.com>
2022-10-13 12:28:45 +0300

    Verification plots (2/N)

--- verification/calc-cf-2d.jl
+++ verification/calc-cf-2d.jl
@@ -6,59 +6,55 @@ using DelimitedFiles
 import Random
 
 function gencenters(side, λ)
-    n = (λ * side^3) |> Poisson |> rand
-    return reduce(hcat, (rand(1:side, 3) for i in 1:n))
+    n = (λ * side^2) |> Poisson |> rand
+    return reduce(hcat, (rand(1:side, 2) for i in 1:n))
 end
 
-function genballs(side, R, λ)
-    sphere_side = 2R + 1
-    padded_side = side + sphere_side
-    spheres = zeros(Int8, (padded_side, padded_side, padded_side))
-    sphere = zeros(Int8, (sphere_side, sphere_side, sphere_side))
+function gendisks(side, R, λ)
+    spheres = zeros(Int8, (side + 2R + 1, side + 2R + 1))
+    sphere  = zeros(Int8, (2R + 1, 2R + 1))
     centers = gencenters(side, λ)
     for i in -R:R
         for j in -R:R
-            for k in -R:R
-                dist = i^2 + j^2 + k^2
-                if dist < R^2
-                    sphere[k+R+1, j+R+1, i+R+1] = 1
-                end
+            dist = i^2 + j^2
+            if dist < R^2
+                sphere[j+R+1, i+R+1] = 1
             end
         end
     end
-
     for center in (centers[:,i] for i in 1:size(centers,2))
         x = center[1]
         y = center[2]
-        z = center[3]
-        spheres[x:x + 2R, y:y + 2R, z:z + 2R] .|= sphere
+        spheres[x:x + 2R, y:y + 2R] .|= sphere
     end
-    return spheres[R+1:end-R-1, R+1:end-R-1, R+1:end-R-1]
+    return spheres[R+1:end-R-1, R+1:end-R-1]
 end
 
 s2_theory(r, R, λ) =
-    let a = (r > 2R) ? 8/3*R^3 : (4/3*R^3 + r*R^2 - r^3/12);
-        exp(-λ*π*a)
-    end
+    let A = 4R^2 - r^2;
+        a = (r < 2R) ? π*R^2 + r*sqrt(A)/4 - acos(r/(2R))*R^2 : π*R^2;
+        exp(-2λ*a)
+end
 
 ss_theory(r, R, λ) =
-    let η = 4/3*λ*π*R^3;
-        a = (r > 2R) ? 9η^2/R^2 : (9η^2/R^2*(1/2 + r/(4R))^2 + 3η/(2r*R))
-        a*s2_theory(r, R, λ)
+    let A = 4R^2 - r^2;
+        a = (r < 2R) ?
+            (A*R^2*λ^2*r*(acos(r/(2R))^2 - 2π*acos(r/(2R)) + π^2) + sqrt(A)*R^2*λ)/(A*r) : (π*λ*R)^2
+        4a*s2_theory(r, R, λ)
     end
 
 sv_theory(r, R, λ) =
-    let η = 4/3*λ*π*R^3;
-        a = (r > 2R) ? 1 : (1/2 + r/(4R))
-        3η/R*a*s2_theory(r, R, λ)
+    let A = 4R^2 - r^2;
+        a = (r < 2R) ? π - acos(r/(2R)) : π
+        2R*λ*a*s2_theory(r, R, λ)
     end
 
 l2_theory(r, R, λ) =
-    exp(-λ*π * (4/3*R^3 + r*R^2))
+    exp(-λ*(π*R^2 + 2r*R))
 
-cl_theory(r, R, λ) = π*λ*R^2*exp(-π*λ*r*R^2)
+cl_theory(r, R, λ) = 2λ*R*exp(-2λ*r*R)
 
-ps_theory(r, R, λ) = 4π*λ*(r + R)^2 * exp(-4/3*π*λ* (r^3 + 3r^2*R + 3r*R^2))
+ps_theory(r, R, λ) = 2λ*π*(r + R)*exp(-λ*π*(r^2 + 2r*R))
 
 const fns = [
     ("s2", s2_theory,
@@ -82,17 +78,17 @@ const hists = [
 ]
 
 function main!()
-    side = 500
-    R = 10
-    λ = 4e-4
+    side = 10000
+    R = 40
+    λ = 5e-5
 
     Random.seed!(13435)
-    data = genballs(side, R, λ)
+    data = gendisks(side, R, λ)
     println(sum(data)/length(data))
     for (name, theory, impl) in fns
         calc = data |> impl |> mean
         th   = theory.(0:length(calc)-1, R, λ)
-        open("$(name)-3d.dat", "w") do out
+        open("$(name)-2d.dat", "w") do out
             writedlm(out, hcat(calc,th))
         end
     end
@@ -100,10 +96,10 @@ function main!()
     for (name, theory, impl) in hists
         calc = data |> impl
         xs = range(minimum(calc), maximum(calc), 1000)
-        open("$(name)-3d-hist.dat", "w") do out
+        open("$(name)-2d-hist.dat", "w") do out
             writedlm(out, calc)
         end
-        open("$(name)-3d-prob.dat", "w") do out
+        open("$(name)-2d-prob.dat", "w") do out
             writedlm(out, hcat(xs, theory.(xs, R, λ)))
         end
     end
